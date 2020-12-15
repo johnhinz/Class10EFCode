385b235cde234cfb3348b07a16189ca4a7ebf93a
jhinz <jhinz402@hotmail.com>
2020-12-14 21:05:06 -0500

    Initial Commit

---
+++ ConsoleApp10/Migrations/20201215012524_Added DbSets.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ConsoleApp10.Migrations
+{
+    public partial class AddedDbSets : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teachers",
+                columns: table => new
+                {
+                    MumboJumo = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teachers", x => x.MumboJumo);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Fee = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    TeacherMumboJumo = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    StudentPocoId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Courses_Students_StudentPocoId",
+                        column: x => x.StudentPocoId,
+                        principalTable: "Students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Courses_Teachers_TeacherMumboJumo",
+                        column: x => x.TeacherMumboJumo,
+                        principalTable: "Teachers",
+                        principalColumn: "MumboJumo",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_StudentPocoId",
+                table: "Courses",
+                column: "StudentPocoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_TeacherMumboJumo",
+                table: "Courses",
+                column: "TeacherMumboJumo");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Courses");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Teachers");
+        }
+    }
+}
---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    public class SchoolContext : DbContext
+    {
+
+        public DbSet<CoursePoco> Courses { get; set; }
+        public DbSet<StudentPoco> Students { get; set; }
+        public DbSet<TeacherPoco> Teachers { get; set; }
+        public DbSet<CourseStudentPoco> CourseStudents { get; set; }
+
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Data Source=CSHARPHUMBER\HUMBERBRIDGING;Initial Catalog=SCHOOL;Integrated Security=True;"
+                );
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(
+            ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<CourseStudentPoco>()
+                .HasKey(cs => new { cs.Student, cs.Course }); 
+
+            modelBuilder.Entity<TeacherPoco>()
+                .HasKey(t => t.MumboJumo );
+            modelBuilder.Entity<StudentPoco>()
+                .HasKey(s => s.Id);
+
+            modelBuilder.Entity<CourseStudentPoco>()
+                .HasOne(cs => cs.Student)
+                .WithMany(s => s.Courses)
+                .HasForeignKey(cs => cs.StudentId);
+
+            modelBuilder.Entity<CourseStudentPoco>()
+                .HasOne(cs => cs.Course)
+                .WithMany(c => c.Students)
+                .HasForeignKey(cs => cs.CourseId);
+
+            modelBuilder.Entity<CourseStudentPoco>()
+                .HasOne<TeacherPoco>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+
+    public class CourseStudentPoco
+    {
+        public Guid CourseId { get; set; }
+        public Guid StudentId { get; set; }
+        public StudentPoco Student { get; set; }
+        public CoursePoco Course { get; set; }
+    }
+
+    // courses
+    public class CoursePoco
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal Fee { get; set; }
+        public List<CourseStudentPoco> Students { get; set; }
+    }
+
+    // teachers 
+    public class TeacherPoco
+    {
+        public Guid MumboJumo { get; set; }
+        public string Name { get; set; }
+        List<CoursePoco> Courses { get; set; }
+    }
+
+    // students
+    public class StudentPoco
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public List<CourseStudentPoco> Courses { get; set; }
+
+    }
+
+}
